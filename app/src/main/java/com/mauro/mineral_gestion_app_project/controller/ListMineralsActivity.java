11a9e285068ec06c2b058de815873ab201ad093d
LoicCaudron <Loic.CAUDRON@student.umons.ac.be>
2018-06-08 13:01:15 +0200

    Suppression d'un commentaire

--- app/src/main/java/com/mauro/mineral_gestion_app_project/controller/ListMineralsActivity.java
+++ app/src/main/java/com/mauro/mineral_gestion_app_project/controller/ListMineralsActivity.java
@@ -59,7 +59,7 @@ public class ListMineralsActivity extends AppCompatActivity {
                 mineralDetailIntent.putExtra("idUser", fk_user);
                 startActivity(mineralDetailIntent);
 
-                // j'ai une erreur sur l'id que tu envoies
+                
             }
         });
 
